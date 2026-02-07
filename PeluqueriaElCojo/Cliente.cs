cc6d8a4f23f7633680c4137d10a538eb49ec597b
ANGEL DAVID MATURE CARABALLO <am2024-2048@uce.edu.do>
2026-02-07 17:54:45 -0400

    actualizacion

---
+++ PeluqueriaElCojo/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeluqueriaElCojo
+{
+    public class Cliente
+    {
+        private string _nombre;
+        private string _telefono;
+        private string _visitas;
+        private static int _contador = 0;
+
+        public int Id {  get; private set; }
+
+        public string Nombre
+
+        {
+            get { return _nombre; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Nombre requerido");
+                _nombre = value.Trim();
+            }
+        }
+
+        public string Telefono
+            { get { return _telefono; }
+            set
+            {
+                string limpio = value.Replace("-", "").Replace(" ", "");
+                if (limpio.Length != 10)
+                    throw new ArgumentException("Telefono: 10 digitos");
+                _telefono = limpio;
+            }
+
+
+
+        }
+    }
+
+}
---
+++ enum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeluqueriaElCojo.Modelos
+public class TipoCliente
+{
+	public enum Tipocliente
+	{
+		Nuevo,
+		Regular,
+		VIP
+	}
+}
