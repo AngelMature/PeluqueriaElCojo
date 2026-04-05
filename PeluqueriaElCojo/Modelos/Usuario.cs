5e72a7f88f942a085f783d33ab84de6ff16c1243
Angel David Mature Caraballo <david12caraballo@gmail.com>
2026-04-05 17:03:52 -0400

    Añadir clase Usuario y Enums para control de privilegios y login AngelM

---
+++ PeluqueriaElCojo/Modelos/Usuario.cs
@@ -0,0 +1,23 @@
+﻿namespace PeluqueriaElCojo.Modelos  //Añadir clase Usuario y Enums para control de privilegios y login
+{
+    public enum RolSistema { Administrador, Empleado }
+
+    public class Usuario
+    {
+        public string Login { get; set; }
+        private string Password;
+        public RolSistema Rol { get; set; }
+
+        public Usuario(string user, string pass, RolSistema rol)
+        {
+            this.Login = user;
+            this.Password = pass;
+            this.Rol = rol;
+        }
+
+        public bool ValidarPassword(string intento)
+        {
+            return this.Password == intento;
+        }
+    }
+}
